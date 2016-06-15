41b0a765a64fb81c2368bce9036ad425ad219e67
dstocco <diego.stocco@cern.ch>
2016-06-15 10:57:11 +0200

    Update QA script: use function (easier to remove some part of it)
    Also, write on slides the runs good for logbook but not in QA

--- QA/getFilesAndMakeSlides.sh
+++ QA/getFilesAndMakeSlides.sh
@@ -31,33 +31,51 @@ if [[ "$EXIT" -eq 1 ]]; then
   exit 2
 fi
 
-if [ -z $ALICE_PHYSICS ]; then
-  echo "Please set ALICE_PHYSICS environement"
-  exit
-fi
-
 
 subDir="$1"
+texFile="muonQA.tex"
+backupTexFile="$texFile.backup"
+runListQA="runListQA.txt"
+runListLogbook="runListLogbook.txt"
 
-if [ -z $subDir ]; then
-  echo "Please enter specific: dataType/year/period/pass"
-  read subDir
-fi
+function MakeDir()
+{
+  local dirName="$1"
+  if [ ! -d "$dirName" ]; then
+    mkdir -p "$dirName"
+  fi
+}
+
+function GetPeriod()
+{
+  echo $(basename $(dirname "$subDir"))
+}
+
+function GetPass()
+{
+  echo $(basename "$subDir")
+}
+
+function SetupDir()
+{
+  if [ -z "$subDir" ]; then
+    echo "Please enter specific: dataType/year/period/pass"
+    read subDir
+  fi
+}
 
 
-#### Syncronize with remote directory
-baseLocalDir="$PWD"
-inputDir="${baseRemoteDir}/$subDir"
-localDir="${baseLocalDir}/$subDir"
+function GetRemoteQAFiles()
+{
+  #### Syncronize with remote directory
 
+  local baseRemote="$1"
+  local relPath="$2"
 
-if [ ! -d "$localDir" ]; then
-  mkdir -p "$localDir"
-fi
-
-cd $localDir
-isNew=0
-if [ ${baseRemoteDir:0:4} = "http" ]; then
+  local inputDir="${baseRemote}/$relPath"
+  #isNew=0
+  if [ ${baseRemoteDir:0:4} = "http" ]; then
+    local currDir="$PWD"
 #  fileList="QA_muon_tracker.pdf QA_muon_tracker.root QA_muon_trigger.pdf QA_muon_trigger.root"
 #  for ifile in $fileList; do
 #    tmpFile="tmp_$ifile"
@@ -68,125 +86,154 @@ if [ ${baseRemoteDir:0:4} = "http" ]; then
 #    else
 #      rm $tmpFile
 #    fi
-  curl -RO "${inputDir}/QA_muon_{tracker,trigger}.{pdf,root}"
-else
-  rsync -avu --exclude="00*" --exclude="*.log" $inputDir/ $localDir/
-fi
+    cd $relPath
+    curl -RO "${inputDir}/QA_muon_{tracker,trigger}.{pdf,root}"
+    cd $currDir
+  else
+    rsync -avu --exclude="00*" --exclude="*.log" $inputDir/ $relPath/
+  fi
 
-trackerFile="QA_muon_tracker.pdf"
-if [ ! -e "$trackerFile" ]; then
-  echo "Problems in downloading files..."
-  exit
-fi
+  trackerFile="$relPath/QA_muon_tracker.pdf"
+  if [ ! -e "$trackerFile" ]; then
+    echo "Problems in downloading files..."
+    retrun 1
+  fi
+
+  return 0
+}
 
 
-pass=$(basename "$subDir")
-period=$(basename $(dirname "$subDir"))
-texFile="muonQA.tex"
-
-oldTexFile="$texFile.backup"
-
-if [ -e "$texFile" ]; then
-  echo "Updating existing latex file"
-  mv $texFile $oldTexFile
+function BackupLatex()
+{
+  if [ -e "$texFile" ]; then
+    echo "Updating existing latex file"
+    mv $texFile $backupTexFile
 #  if [ $forceLatex -eq 0 ]; then
 #    echo "Latex file already present. It will not be re-created"
 #  else
 #    rm $texFile
 #  fi
-fi
-
-
-##### Make slides
-#if [ ! -e "$texFile" ]; then
-
-##### Ask for a list of triggers
-if [ -z $triggerList ]; then
-  if [ -e "$oldTexFile" ]; then
-    triggerList=$(grep "%TriggerList=" $oldTexFile | cut -d "=" -f2)
   fi
+}
+
+
+function MakeQASlides() {
+
+  if [ -z $ALICE_PHYSICS ]; then
+    echo "Please set ALICE_PHYSICS environement"
+    return 1
+  fi
+
+  BackupLatex
+  local period="$(GetPeriod)"
+  local pass="$(GetPass)"
+
+  ##### Ask for a list of triggers
   if [ -z $triggerList ]; then
-    open "$trackerFile"
-    echo "Trigger list (comma separated) for $PWD"
-    read triggerList
+    if [ -e "$backupTexFile" ]; then
+      triggerList=$(grep "%TriggerList=" $backupTexFile | cut -d "=" -f2)
+    fi
+    if [ -z $triggerList ]; then
+      open "QA_muon_tracker.pdf"
+      echo "Trigger list (comma separated) for $PWD"
+      read triggerList
+    fi
   fi
-fi
 
-root -b -q $ALICE_PHYSICS/PWGPP/MUON/lite/MakeSlides.C+\(\"$period\"\,\"$pass\",\"$triggerList\",\""$authors"\",\"QA_muon_tracker.pdf\",\"QA_muon_trigger.pdf\",\"$texFile\"\)
-issueFound=$(grep "page=-1" $texFile)
-if [ "$issueFound" != "" ]; then
-  echo "Problem in $texFile : one page was not found :"
-  echo "$issueFound"
-  echo "Please check the file and fix the issue (e.g. by removing the slide)"
-  open $texFile
+  root -b -q $ALICE_PHYSICS/PWGPP/MUON/lite/MakeSlides.C+\(\"$period\"\,\"$pass\",\"$triggerList\",\""$authors"\",\"QA_muon_tracker.pdf\",\"QA_muon_trigger.pdf\",\"$texFile\"\)
+  issueFound=$(grep "page=-1" $texFile)
+  if [ "$issueFound" != "" ]; then
+    echo "Problem in $texFile : one page was not found :"
+    echo "$issueFound"
+    echo "Please check the file and fix the issue (e.g. by removing the slide)"
+    open $texFile
+    answer="n"
+    while [ "$answer" != "y" ]; do
+      echo "Press y when done"
+      read answer
+    done
+  fi
+
+  if [ ! -e "$texFile" ]; then
+    echo "Something wrong: latex file not produced..."
+    if [ -e $backupTexFile ]; then
+      mv $backupTexFile $texFile
+    fi
+    return 1
+  fi
+  return 0
+}
+
+
+function MakeRunListQA()
+{
+  ##### Get analyzed runs from QA
+  grep "runTab" $texFile | grep -oE "{[0-9]+}" | sed 's/{//;s/}//' > $runListQA
+}
+
+function GetRunListQA()
+{
+  local redo="$1"
+  if [[ ! -e $runListQA || $redo -eq 1 ]]; then
+    MakeRunListQA
+  fi
+  cat $runListQA
+}
+
+
+function MakeRunListLogbook()
+{
+  ##### Get run list from logbook
+  local period="$1"
+  local logbookUrl="https://alice-logbook.cern.ch/logbook/date_online.php?p_cont=sb&p_rspn=1&p_rsob=l.run&p_rsob_dir=DESC&ptcf_rtc=%2CAt+least%2CMUON_TRG%3BMUON_TRK%3B%2CAt+least&prsf_rtype=PHYSICS%2C&prsf_rdur=10+m%2C%2C&pqff_det_MUON_TRK=Bad+run%2C1&pqff_det_MUON_TRG=Bad+run%2C1&prsf_rgmr=Yes&pqff_rq=Bad+run%2C1&psf_sd=Yes&psf_det_GLOBAL=DONE%2C0&psf_det_MUON_TRK=DONE%2C0&psf_det_MUON_TRG=DONE%2C0&pbf_bm=STABLE%2C&prsf_rlp=$period%2C"
+
+  which xdg-open > /dev/null 2>&1
+  if [[ $? == 0 ]]; then
+    xdg-open "$logbookUrl"
+  else
+    open "$logbookUrl"
+  fi
+
+  touch $runListLogbook
+  open "$runListLogbook"
   answer="n"
   while [ "$answer" != "y" ]; do
+    echo "Please write the run list from the logbook in $runListLogbook"
     echo "Press y when done"
     read answer
   done
+}
 
-fi
-#fi
 
-if [ ! -e "$texFile" ]; then
-  echo "Something wrong: latex file not produced..."
-  if [ -e $oldTexFile ]; then
-    mv $oldTexFile $texFile
+function SummaryFromExistingSlides()
+{
+  ##### If old tex file existed, update summary and run-by-run information
+  if [ ! -e "$backupTexFile" ]; then
+    return 1;
   fi
-  cd $baseLocalDir
-  exit
-fi
-
-
-
-##### Get analyzed runs from QA
-runListQA="runListQA.txt"
-grep "runTab" $texFile | grep -oE "{[0-9]+}" | sed 's/{//;s/}//' > $runListQA
-
-
-##### Get run list from logbook
-runListLogbook="runListLogbook.txt"
-logbookUrl="https://alice-logbook.cern.ch/logbook/date_online.php?p_cont=sb&p_rspn=1&p_rsob=l.run&p_rsob_dir=DESC&ptcf_rtc=%2CAt+least%2CMUON_TRG%3BMUON_TRK%3B%2CAt+least&prsf_rtype=PHYSICS%2C&prsf_rdur=10+m%2C%2C&pqff_det_MUON_TRK=Bad+run%2C1&pqff_det_MUON_TRG=Bad+run%2C1&prsf_rgmr=Yes&pqff_rq=Bad+run%2C1&psf_sd=Yes&psf_det_GLOBAL=DONE%2C0&psf_det_MUON_TRK=DONE%2C0&psf_det_MUON_TRG=DONE%2C0&pbf_bm=STABLE%2C&prsf_rlp=$period%2C"
-
-which xdg-open > /dev/null 2>&1
-if [[ $? == 0 ]]; then
-  xdg-open "$logbookUrl"
-else
-  open "$logbookUrl"
-fi
-
-touch $runListLogbook
-open "$runListLogbook"
-answer="n"
-while [ "$answer" != "y" ]; do
-  echo "Please write the run list from the logbook in $runListLogbook"
-  echo "Press y when done"
-  read answer
-done
-
-
-##### If old tex file existed, update summary and run-by-run information
-if [ -e "$oldTexFile" ]; then
   # Use the summary and ending from the existing file
-  sep1='frametitle{Run summary'
-  sep2='frametitle{Hardware issues}'
+  local sep1='frametitle{Run summary'
+  local sep2='frametitle{Hardware issues}'
 
-  summary="summary_$texFile"
-  cat $oldTexFile | sed -n "1,/$sep1/p" | grep -v "$sep1" > $summary
-  body="body_$texFile"
+  local summary="summary_$texFile"
+  cat $backupTexFile | sed -n "1,/$sep1/p" | grep -v "$sep1" > $summary
+  local body="body_$texFile"
   cat $texFile | sed -n "/$sep1/,/$sep2/p" | grep -v "$sep2" > $body
-  ending="end_$texFile"
-  cat $oldTexFile | sed -n "/$sep2/,/\end{document}/p" > $ending
+  local ending="end_$texFile"
+  cat $backupTexFile | sed -n "/$sep2/,/\end{document}/p" > $ending
 
   cat $summary $body $ending > $texFile
   rm $summary $body $ending
 
-# Update summary run-by-run info
-  runListQAstr=$(cat $runListQA | xargs)
-  changeCommand=""
+  # Update summary run-by-run info
+  if [ ! -e $runListQA ]; then
+    MakeRunListQA
+  fi
+  local runListQAstr=$(cat $runListQA | xargs)
+  local changeCommand=""
   sedCut='s/^[[:space:]]*//;s/\\/\\\\/g;s/\[/\\\[/g;s/\]/\\\]/g;s/\?/\\\?/;s:/:\\/:g;'
   for irun in $runListQAstr; do
-    infoRun=$(grep "runTab" $oldTexFile | grep $irun | grep -v "{}" | sed "$sedCut")
+    infoRun=$(grep "runTab" $backupTexFile | grep $irun | grep -v "{}" | sed "$sedCut")
     if [ "$infoRun" != "" ]; then
       genRun=$(grep "runTab" $texFile | grep $irun | sed "$sedCut")
       changeCommand="${changeCommand}s/$genRun/$infoRun/;"
@@ -198,63 +245,142 @@ if [ -e "$oldTexFile" ]; then
       fi
     fi
   done
-if [ ${#changeCommand} -gt 1 ]; then
-  sed -i '' "$changeCommand" $texFile
-fi
+  if [ ${#changeCommand} -gt 1 ]; then
+    sed -i '' "$changeCommand" $texFile
+  fi
 
 #  mv $tmpTexFile $generatedTexFile
-#  diff --changed-group-format='%<' --old-group-format='%<' --new-group-format='%>' $oldTexFile $generatedTexFile > $tmpTexFile
-fi
-
-inQAandLogbook=`comm -12 <(sort $runListQA) <(sort $runListLogbook) | xargs`
-onlyInQA=`comm -23 <(sort $runListQA) <(sort $runListLogbook) | xargs`
-onlyInLogbook=`comm -13 <(sort $runListQA) <(sort $runListLogbook) | xargs`
+#  diff --changed-group-format='%<' --old-group-format='%<' --new-group-format='%>' $backupTexFile $generatedTexFile > $tmpTexFile
+}
 
 
-##### Change color of table lines for runs not in the logbook
-changeCommand=""
-for irun in $onlyInQA; do
-changeCommand="${changeCommand}s/runTab{$irun}/runTab[\\\notInLogColor]{$irun}/;"
-done
-sed -i "" "$changeCommand" $texFile
+function UpdateWithLogbookInfo()
+{
+  ##### Change color of table lines for runs not in the logbook
+  if [ ! -e $runListLogbook ]; then
+    MakeRunListLogbook
+  fi
+  if [ ! -e $runListQA ]; then
+    MakeRunListQA
+  fi
+
+  local onlyInQA=$(comm -23 <(sort $runListQA) <(sort $runListLogbook) | xargs)
+  local onlyInLogbook=$(comm -13 <(sort $runListQA) <(sort $runListLogbook) | xargs)
+
+  if [[ -z "$onlyInLogbook" && -z "$onlyInQA" ]]; then
+    return 0;
+  fi
+
+  awk -v logOnly="${onlyInLogbook}" -v qaOnly="${onlyInQA}" '
+    BEGIN {
+      isSummarySlide=0
+      isRunSummary=0
+      matchString="In e-logbook, not yet in QA:"
+      outString=matchString " " logOnly
+      foundLogSummary=0
+      nQAonly=split(qaOnly,qaOnlyArr," ")
+    }
+    {
+      if ( index($0,"\\frametitle\{Summary") ) isSummarySlide=1
+      else if ( index($0,"\\frametitle\{Run summary") ) isRunSummary=1
+      isEnd = index($0,"\\end{frame}")
+      output=$0
+
+      if ( isSummarySlide==1 ) {
+        if ( foundLogSummary == 0 ) {
+          if ( index($0,matchString) || isEnd ) {
+            print outString
+            foundLogSummary=1
+            if ( ! isEnd ) next
+          }
+        }
+      }
+      else if ( isRunSummary==1 ) {
+        for ( irun=1; irun<=nQAonly;irun++ ) {
+          if ( index($0,qaOnlyArr[irun]) ) sub("runTab{","runTab[\\notInLogColor]{");
+        }
+      }
+      if ( isEnd ) {
+        isSummarySlide=0
+        isRunSummary=0
+      }
+      print $0;
+    } ' $texFile > $texFile.tmp
+  mv $texFile.tmp $texFile
+}
 
 
-compileTexLog="pdflatex.log"
-echo ""
-echo "Compiling latex:"
-echo "pdflatex $texFile (output in $compileTexLog) ..."
-pdflatex $texFile > $compileTexLog
-pdflatex $texFile >> $compileTexLog
 
-open $texFile
+function PrintRunSummary()
+{
+  if [ ! -e $runListLogbook ]; then
+    MakeRunListLogbook
+  fi
+  if [ ! -e $runListQA ]; then
+    MakeRunListQA
+  fi
 
-echo ""
-echo "Common runs (`echo ${inQAandLogbook} | wc -w | xargs`) :"
-echo "${inQAandLogbook}" | xargs
-echo ""
-echo "only in QA (`echo ${onlyInQA} | wc -w | xargs`) :"
-echo "${onlyInQA}" | xargs
-echo ""
-echo "only in logbook (`echo ${onlyInLogbook} | wc -w | xargs`) :"
-echo "${onlyInLogbook}" | xargs
+  local inQAandLogbook=$(comm -12 <(sort $runListQA) <(sort $runListLogbook) | xargs)
+  local onlyInQA=$(comm -23 <(sort $runListQA) <(sort $runListLogbook) | xargs)
+  local onlyInLogbook=$(comm -13 <(sort $runListQA) <(sort $runListLogbook) | xargs)
 
-#if [ -e "$oldTexFile" ]; then
-#  echo ""
-#  echo "Please check that the old tex file ($oldTexFile) and the generated one ($generatedTexFile)"
-#  echo "were properly merged:"
-#  echo ""
-#  echo "diff $oldTexFile $texFile : "
-#  diff $oldTexFile $texFile
-#  echo ""
-#  echo "diff $generatedTexFile $texFile :"
-#  diff $generatedTexFile $texFile
-#  echo ""
-#  echo "Remove $generatedTexFile $oldTexFile? [y/n]"
-#  read answer
-#  if [ "$answer" = "y" ]; then
-#    rm $generatedTexFile $oldTexFile
-#  fi
-#fi
+  echo ""
+  echo "Common runs (`echo ${inQAandLogbook} | wc -w | xargs`) :"
+  echo "${inQAandLogbook}" | xargs
+  echo ""
+  echo "only in QA (`echo ${onlyInQA} | wc -w | xargs`) :"
+  echo "${onlyInQA}" | xargs
+  echo ""
+  echo "only in logbook (`echo ${onlyInLogbook} | wc -w | xargs`) :"
+  echo "${onlyInLogbook}" | xargs
+}
 
-cd $baseLocalDir
+
+function CompileLatex()
+{
+  local compileTexLog="pdflatex.log"
+  echo ""
+  echo "Compiling latex:"
+  echo "pdflatex $texFile (output in $compileTexLog) ..."
+  pdflatex $texFile > $compileTexLog
+  pdflatex $texFile >> $compileTexLog
+
+  open $texFile
+}
+
+function main() {
+  local baseLocalDir="$PWD"
+  SetupDir
+
+  local localDir="$baseLocalDir/$subDir"
+  MakeDir "$subDir"
+
+  GetRemoteQAFiles "$baseRemoteDir" "$subDir"
+  if [ $? -ne 0 ]; then
+    cd $baseLocalDir
+    return 1
+  fi
+  cd $localDir
+  MakeQASlides
+  if [ $? -ne 0 ]; then
+    cd $baseLocalDir
+    return 1
+  fi
+
+  cd $localDir
+  MakeRunListQA
+  MakeRunListLogbook "$(GetPeriod)"
+
+  SummaryFromExistingSlides
+  UpdateWithLogbookInfo
+
+  CompileLatex
+
+  PrintRunSummary
+
+  cd $baseLocalDir
+  return 0
+}
+
+main
 
